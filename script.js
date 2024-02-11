69775377ed88ae0da0f463be9982b1c34eee8246
dorddord <105528953+dorddord@users.noreply.github.com>
2024-02-11 00:47:05 -0600

    Basic Random HTML and stuff
    yea

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Future JavaScript code for MunchMap will go here
+    console.log("MunchMap is ready to serve!");
+});
